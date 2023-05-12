58faacc5f3f40ce00540320ba934692253ccc8e0
Bander Al-Mutairi <73838482+B6xb@users.noreply.github.com>
2023-05-12 08:16:52 +0300

    ..

--- mosourati/lib/prismadb.js
+++ mosourati/lib/prismadb.js
@@ -4,3 +4,15 @@ const client = global.prismadb || new PrismaClient();
 if (process.env.NODE_ENV === "production") global.prismadb = client;
 
 export default client;
+
+// import { PrismaClient } from "@prisma/client";
+
+// const client = global;
+// client.prismadb = client.prismadb || new PrismaClient({ log: ["query"] });
+// const prismadb = client.prismadb;
+
+// if (process.env.NODE_ENV !== "production") {
+//   client.prismadb = prismadb;
+// }
+
+// export default client;
