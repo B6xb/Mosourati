b745b6fa517f1d0773623c76c52de5531be68e51
Bander Al-Mutairi <73838482+B6xb@users.noreply.github.com>
2023-05-12 10:15:19 +0300

    ..

--- mosourati/pages/api/graphql.js
+++ mosourati/pages/api/graphql.js
@@ -1,3 +1,5 @@
+///// ## BOTH APPROACHES ARE NOT WORKING
+
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
@@ -18,4 +20,36 @@ const server = new ApolloServer({
   typeDefs,
 });
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default startServerAndCreateNextHandler(server);
+
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
+// import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
+
+// const resolvers = {
+//   Query: {
+//     hello: () => "world",
+//   },
+// };
+
+// const typeDefs = `#graphql
+//   type Query {
+//     hello: String
+//   }
+// `;
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   plugins: [ApolloServerPluginLandingPageDisabled()],
+// });
+// const { url } = await startStandaloneServer(server, {
+//   context: async ({ req }) => ({ token: req.headers.token }),
+//   listen: { port: 4000 },
+// });
+// console.log(`🚀  Server ready at ${url}`);
